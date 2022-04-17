db8fd422549f636b2379821daa9de85d93a3ba03
kaushal soni <kaushal95300@gmail.com>
2022-04-17 19:54:18 +0530

    updated code with specific db

--- app.py
+++ app.py
@@ -7,13 +7,17 @@ import pymysql
 class Db:
     def __init__(self):
         print("DB object created")  
-    def set(self,user,passwd,host):
+    def set(self,user,passwd,host,db):
         self.user = user
         self.passwd = passwd
-        self.host = host       
+        self.host = host   
+        self.db = db    
    
     def connect(self):
-        self.db = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd)
+        if self.db is None or self.db == "":
+            self.db = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd)
+        else:
+            self.db = pymysql.connect(host=self.host, user=self.user, passwd=self.passwd, db=self.db)
         return self.db
 
     def command(self,cmd):
@@ -28,25 +32,31 @@ app = Flask(__name__)
 user = "user"
 host = "hoast"
 passwd = "pass"
+database = ""
 db = Db()
+
 @app.route('/')
 @app.route('/index')
 def index():
     try: db.exit()
     except: print("No connection to close")
     return render_template('index.html')
+
 @app.route('/output', methods=['POST'])
 def output():
     global user
     global passwd
     global host
+    global database
     print( user, passwd,host)
     user = request.form['username']
     passwd = request.form['password']
     host = request.form['url']
-    print( user, passwd,host)
-    db.set(user,passwd,host)
+    database = request.form['database']
+    print( user, passwd,host,database)
+    db.set(user,passwd,host,database)
     return render_template('output.html', content="Enter Command to proceed")
+
 @app.route('/result', methods=['POST'])
 def result():
     global user
@@ -56,5 +66,6 @@ def result():
     print( user, passwd,host)
     data = db.command(command)
     return render_template('output.html', content="Output \n\n\n {}".format(data))
+
 if __name__ == '__main__':
     app.run(debug=True)
